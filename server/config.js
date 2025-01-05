7811fb891c376d585727af80789733f1a14d1885
briteming <briteming@gmail.com>
2025-01-04 22:35:51 -0800

    Update config.js

--- server/config.js
+++ server/config.js
@@ -5,6 +5,6 @@ module.exports = {
     keys: [ env.KEY ],
     // keys: ['awhzUxNPNvQ9JeAd8KnVuBsCDSyxR0r4pDL/aupk/iLS6cuRaRBNlQnN7lXFZZTzUiGa2fpvRlwN0V9S+EWJPg==']
     // keys: [ crypto.pseudoRandomBytes(64).toString('base64') ]
-    client_id: env.CLIENT_ID,
-    client_secret: env.CLIENT_SECRET
+    client_id: env.Ov23liq0VjxWPMAVb8Ig,
+    client_secret: env.d9ca3c22bf57be1920a5cf47c4baa7c960981d36
 };
